f832d81053507b0764427684afba42e3c0307493
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2025-12-03 16:01:31 -0300

    primeiro
    mudanças

--- config.php
+++ config.php
@@ -6,10 +6,12 @@ define('DB_NAME', 'finacia');
 define('DB_USER', 'root');
 define('DB_PASS', 'mysql');
 
+
+
 //conexão com o Banco de Dados 
 try {
-    $conn = new PDO("mysql:hot=$DB_HOST; port=$DB_PORT;debname=$DB_NAME;
-    charset=utf8mb4",$DB_USER,$DB_PASS);
+    $conn = new PDO("mysql:hot=".DB_HOST."; port=".DB_PORT.";debname=".DB_NAME.";
+    charset=utf8mb4",DB_USER,DB_PASS);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO:: ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão com o banco de dados " . $e->getMessage());
