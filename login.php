74468203ee604b26cd16d5f257b035484d108b5f
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2025-12-03 16:54:21 -0300

    segunda parte
    longin

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'config.php';
+
+// verificar se o usuario esta logado
+if(isset($_SESSION['usuario_id'])){
+    header('location:index.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>longin</title>
+</head>
+<body>
+    <h1>longin - Sistema Financeiro</h1>
+    <form action="autenticae" method="post">
+        <div>
+            <label for="email">E-mail:</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+         <div>
+            <label for="senha">Senha:</label>
+            <input type="passoword" name="senha" id="senha" required>
+        </div>
+         <div>
+            <button type="submit">Entrar</button>
+        </div>
+    </form>
+
+    <p>Não tem conta? <a href="registro.php">Cdastre-se aqui.</a></p>
+</body>
+</html>
